c4fdc13d72fbde37fcb5937556cd96bd07a6bc0b
andrewtweber <andrew@midwestscc.com>
2018-12-18 21:48:33 -0600

    Image view

---
+++ Popping/Controls/ImageView.swift
@@ -0,0 +1,33 @@
+//
+//  ImageView.swift
+//  Popping
+//
+//  Created by Andrew Weber on 12/18/18.
+//  Copyright © 2018 Andrew Weber. All rights reserved.
+//
+
+import UIKit
+
+class ImageView: UIControl
+{
+    var imageView: UIImageView?
+    var image: UIImage?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.layer.cornerRadius = 5
+        self.layer.masksToBounds = true
+        
+        self.imageView = UIImageView(frame: self.bounds)
+        self.addSubview(imageView!)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func setImage(_ image: UIImage) {
+        self.imageView?.image = image
+        self.image = image
+    }
+}
